6822b6d0bbd0e7867437e006d0a205c5dbe6f6de
alexdietz1988 <alex.dietz@gmail.com>
2022-06-24 12:27:39 -0400

    Remove create/edit/delete features for now - I'll just use admin portal

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,13 +7,6 @@ urlpatterns = [
     path('publications', views.Publications.as_view(), name="publications"),
 
     path('blog', views.Blog.as_view(), name="blog"),
-    path('tags', views.TagList.as_view(), name="tag-list"),
     path('tag/<int:pk>', views.TaggedPosts.as_view(), name="tagged-posts"),
     path('post/<int:pk>', views.PostDetail.as_view(), name="postdetail"),
-    path('post/<int:pk>/edit', views.PostUpdate.as_view(), name="post-update"),
-    path('post/<int:pk>/delete', views.PostDelete.as_view(), name="postdelete"),
-    path('post/new', views.PostCreate.as_view(), name="post-create"),
-    path('tag/new', views.TagCreate.as_view(), name="tag-create"),
-    path('tag/<int:pk>/rename', views.TagUpdate.as_view(), name="tag-update"),
-    path('tag/<int:pk>/delete', views.TagDelete.as_view(), name="tag-delete")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,4 @@
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post, Tag
 
 # Create your views here.
@@ -22,57 +21,23 @@ class Blog(TemplateView):
         return context
 
 class PostDetail(TemplateView):
-    template_name = "blog/posts/post-detail.html"
+    template_name = "blog/post-detail.html"
 
     def get_context_data(self, pk, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post'] = Post.objects.get(pk=pk)
         return context
 
-class PostCreate(CreateView):
-    model = Post
-    fields = ['title', 'body', 'tags']
-    template_name = "blog/posts/post-create.html"
-    success_url = "/blog"
-
-class PostUpdate(UpdateView):
-    model = Post
-    fields = ['title', 'body', 'tags']
-    template_name = "blog/posts/post-update.html"
-    success_url = "/blog"
-
-class PostDelete(DeleteView):
-    model = Post
-    template_name = "blog/posts/post-delete.html"
-    success_url = "/blog"
-
 class TagList(TemplateView):
-    template_name = "blog/tags/tag-list.html"
+    template_name = "blog/tag-list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tags'] = Tag.objects.all()
         return context
 
-class TagCreate(CreateView):
-    model = Tag
-    fields = ['name']
-    template_name = "blog/tags/tag-create.html"
-    success_url = "/tags"
-
-class TagUpdate(UpdateView):
-    model = Tag
-    fields = ['name']
-    template_name = "blog/tags/tag-update.html"
-    success_url = "/tags"
-
-class TagDelete(DeleteView):
-    model = Tag
-    template_name = "blog/tags/tag-delete.html"
-    success_url = "/tags"
-
 class TaggedPosts(TemplateView):
-    template_name = "blog/tags/tagged-posts.html"
+    template_name = "blog/tagged-posts.html"
 
     def get_context_data(self, pk, **kwargs):
         context = super().get_context_data(**kwargs)
